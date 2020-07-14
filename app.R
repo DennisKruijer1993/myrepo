65d60aacc2174303b7a3ee98bd3f534a421af7b1
DennisKruijer1993 <dennis.kruijer@isatis-business-solutions.nl>
2020-07-14 14:52:23 +0200

    advanced graph styling

--- app.R
+++ app.R
@@ -1,13 +1,38 @@
 ## A basic shiny app
 
 library(shiny)
+library(ggplot2)
+library(DT)
 
 server <- function(input, output, session){
   # input <- interactive part (e.g. buttons, sliders, textfield, etc.)
   # output <- calculation
   # session <- session information (if no reactivity, you don't need 'session')
+  
+  # Plot
+  output$plot <- renderPlot({
+    ggplot(data = diamonds,
+           mapping = aes(price, carat)) + geom_point()
+  })
+  
+  # Brush
+  diam <- reactive({
+    user_brush <- input$user_brush
+    # user_click <- input$user_click
+    sel <- brushedPoints(diamonds, user_brush)
+    # sel <- nearPoints(diamonds, user_click, theshold = 10, maxpoints = 5)
+    return(sel)
+  })
+  
+  # Table
+  output$table <- DT:: renderDataTable(DT::datatable(diam()))
 }
 
-ui <- basicPage("our first basic app")
+ui <- fluidPage(
+  h1("Using the brush feature to select specific observations"),
+  plotOutput(outputId = "plot", brush = "user_brush"),
+  # plotOutput(outputId = "plot", click = "user_click"),
+  dataTableOutput(outputId = "table")
+)
 
 shinyApp(ui = ui, server = server)
