02c343a2b3079ea98768047ecc65a686c8ba0945
DennisKruijer1993 <dennis.kruijer@isatis-business-solutions.nl>
2020-07-14 15:30:18 +0200

    media tags

--- app.R
+++ app.R
@@ -8,6 +8,26 @@ server <- function(input, output, session){
   # session <- session information (if no reactivity, you don't need 'session')
 }
 
-ui <- basicPage("our first basic app")
+ui <- navbarPage(title = "Integration of different media types",
+                 
+                 tabPanel("Image sourced locally",
+                          tags$img(src = "logo.png",
+                                   width = "100px",
+                                   height = "100px")),
+                 
+                 tabPanel("Video sourced locally",
+                          tags$video(src = "video.mp4",
+                                     type = "video/mp4",
+                                     controls = T,
+                                     width = "900px", 
+                                     hieght = "800px")),
+                 
+                 tabPanel("Pdf source online, Iframe",
+                          tags$iframe(style = "height:600px; width:100%; scrolling=yes",
+                                      src = "https://cran.r-project.org/web/packages/shiny/shiny.pdf")),
+                 
+                 tabPanel("Text as .txt",
+                          includeText("mytxt.txt"))
+)
 
 shinyApp(ui = ui, server = server)
