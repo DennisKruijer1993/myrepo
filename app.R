ae7d91016eb8ab205bcbdf17d3774547227cc15f
DennisKruijer1993 <dennis.kruijer@isatis-business-solutions.nl>
2020-07-14 13:35:55 +0200

    bugfix

--- app.R
+++ app.R
@@ -2,12 +2,12 @@
 
 library(shiny)
 
-server = function(input, output, session){
+server <- function(input, output, session){
   # input <- interactive part (e.g. buttons, sliders, textfield, etc.)
   # output <- calculation
   # session <- session information (if no reactivity, you don't need 'session')
 }
 
-ui = basicPage("our first basic app")
+ui <- basicPage("our first basic app")
 
 shinyApp(ui = ui, server = server)
