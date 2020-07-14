0998e2f51aed3cbf721d2df1ad37df0b694fe584
DennisKruijer1993 <dennis.kruijer@isatis-business-solutions.nl>
2020-07-14 11:17:29 +0200

    reactiverender

--- app.R
+++ app.R
@@ -2,12 +2,23 @@
 
 library(shiny)
 
-server = function(input, output, session){
+ui <- basicPage(
+  h1("Using Reactive"),
+  sliderInput(inputId = "myslider", label = "slider1", value = 1, min = 1, max = 20),
+  plotOutput(outputId = "plot")
+)
+
+server <- function(input, output, session){
   # input <- interactive part (e.g. buttons, sliders, textfield, etc.)
   # output <- calculation
   # session <- session information (if no reactivity, you don't need 'session')
+  data <- reactive({
+    rnorm(50) * input$myslider
+  })
+  
+  output$plot <- renderPlot({
+    plot(data(), col = "red", pch = 21, bty = "n")
+  })
 }
-
-ui = basicPage("our first basic app")
 
 shinyApp(ui = ui, server = server)
