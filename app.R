5736de425e83e31243e334f3a9a15d4e8a7a6129
DennisKruijer1993 <dennis.kruijer@isatis-business-solutions.nl>
2020-07-14 12:49:31 +0200

    layout

--- app.R
+++ app.R
@@ -2,12 +2,31 @@
 
 library(shiny)
 
-server = function(input, output, session){
+server <- function(input, output, session){
   # input <- interactive part (e.g. buttons, sliders, textfield, etc.)
   # output <- calculation
   # session <- session information (if no reactivity, you don't need 'session')
 }
 
-ui = basicPage("our first basic app")
+ui <- fluidPage(
+  
+  titlePanel("using Tabsets"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId = "s1",
+                  label = "My Slider",
+                  value = 1, min = 1, max = 20)
+    ),
+  
+  mainPanel(
+    tabsetPanel(
+      tabPanel(title = "Tab1", "First Tab"),
+      tabPanel(title = "Tab2", "Second Tab"),
+      tabPanel(title = "Tab3", "Third tab")
+      )
+    )
+  )
+)
 
 shinyApp(ui = ui, server = server)
