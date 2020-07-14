66bccab8fc66f6e103a313c221d6058bd9b5a8b1
DennisKruijer1993 <dennis.kruijer@isatis-business-solutions.nl>
2020-07-14 13:14:41 +0200

    htmltags

--- app.R
+++ app.R
@@ -2,12 +2,38 @@
 
 library(shiny)
 
-server = function(input, output, session){
+server <- function(input, output, session){
   # input <- interactive part (e.g. buttons, sliders, textfield, etc.)
   # output <- calculation
   # session <- session information (if no reactivity, you don't need 'session')
 }
 
-ui = basicPage("our first basic app")
+ui <- fluidPage(
+  
+  titlePanel(strong("This is the STRONG tag on the Title")),
+  
+  sidebarLayout(
+    sidebarPanel(
+      withTags(  ## Using withTags allows you to omit tags$*html tag*
+        div(
+          b("bold text: here you see a line break, horizontal line and code"),
+          br(),
+          hr(),
+          code("plot(lynx)")
+        )
+      )
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel(title = "Weblinks with direct tag a",
+                 value = a(href="www.r-tutorials.com", "r-tutorials")),
+        tabPanel(title = tags$b("Using b for bold text"),  ## tags$b to gain bold text is necessary
+                 value = tags$b("a bold text")),
+        tabPanel("citatations with the blockquote tag", 
+                 tags$blockquote("R is Great", cite = "R programmer"))
+      )
+    )
+  )
+)
 
 shinyApp(ui = ui, server = server)
