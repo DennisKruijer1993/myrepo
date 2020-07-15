f2c6ddebc3dd27b036448dd81b99a20d34231c62
DennisKruijer1993 <dennis.kruijer@isatis-business-solutions.nl>
2020-07-15 11:26:54 +0200

    added navbar

--- app.R
+++ app.R
@@ -1,10 +1,31 @@
-## A basic shiny app
+### Requirements
+## themeSelector
+## Title: The Mining Stock Scale
+## Three tabs: navbarPage
+## First tab: 
+# sliders -> plot with brush -> data table (use reactive)
+# calculation: G1*weight1 + G2*weight2 + G3*weight3
+# sliders: "wellPanel" different scales
+# plot is made with ggplot2
+# Downloadbutton
+## Documentation
+# Insert some documentation
+# PDF or video (iframe)
+## Data table
+# currency
+# grade fileds (ligthblue background)
+# Symbol field: grey font
 
 library(shiny)
 library(DT)
+library(ggplot2)
 library(shinythemes)
 
-ui <- fluidPage(shinytheme(),
+ui <- fluidPage(themeSelector(),
+                navbarPage(title = "The Mining Stock Scale",
+                           tabPanel(title = "ADJUST YOUR MINING STOCKS"),
+                           tabPanel(title = "DOCUMENTATION"),
+                           tabPanel(title = "DATA TABLE WITH UNDERLYING DATA"))
                 
   
 )
