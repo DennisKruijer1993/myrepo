fe7b8793ddbdd02e1b7775bbf0d271a82cde2d81
DennisKruijer1993 <dennis.kruijer@isatis-business-solutions.nl>
2020-07-15 11:35:45 +0200

    Added sliders

--- app.R
+++ app.R
@@ -23,8 +23,40 @@ library(shinythemes)
 
 ui <- fluidPage(theme = shinytheme("sandstone"),
                 navbarPage(title = "The Mining Stock Scale",
-                           tabPanel(title = "ADJUST YOUR MINING STOCKS"),
+                           
+                           # Tab 1
+                           tabPanel(title = "ADJUST YOUR MINING STOCKS",
+                                    wellPanel(
+                                      # Sliders
+                                      sliderInput(inputId = "weight1",
+                                                  label = "Weight on Grade 1",
+                                                  min = 0,
+                                                  max = 20,
+                                                  value = 0,
+                                                  step = 2),
+                                      sliderInput(inputId = "weight2",
+                                                  label = "Weight on Grade 2",
+                                                  min = 0,
+                                                  max = 20,
+                                                  value = 0,
+                                                  step = 2),
+                                      sliderInput(inputId = "weight3", 
+                                                  label = "Weight on Grade 3",
+                                                  min = 0,
+                                                  max = 6,
+                                                  value = 0,
+                                                  step = 0.6)
+                                    ),
+                                    
+                                    # Plot
+                                    plotOutput(outputId = "plot", 
+                                               brush = brushOpts(id = "plot_brush"))
+                                    ),
+                           
+                           # Tab 2
                            tabPanel(title = "DOCUMENTATION"),
+                           
+                           # Tab 3
                            tabPanel(title = "DATA TABLE WITH UNDERLYING DATA"))
                 
   
